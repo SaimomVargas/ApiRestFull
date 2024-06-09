c4cd438d787b0505295ca295c9f464dec11752f3
Saimom Vargas <saimonmatheus@gmail.com>
2024-06-08 21:31:27 -0300

    Projeto ApiResfull

---
+++ src/main/java/dio/dio_first_api/DioFirstApiApplication.java
@@ -0,0 +1,13 @@
+package dio.dio_first_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioFirstApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioFirstApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dio/dio_first_api/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package dio.dio_first_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcome(){
+        return "Oie caralho";
+    }
+}
